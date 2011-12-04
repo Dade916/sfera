eee41aa9c6780186f8711b51b9c86df617fcee1a
David <davibu@interfree.it>
2011-12-04 17:08:43 +0100

    Working on Windows compatibility

--- cmake/SpecializedConfig/Config_Dade-win64.cmake
+++ cmake/SpecializedConfig/Config_Dade-win64.cmake
@@ -5,8 +5,8 @@
 #
 ###########################################################################
 
-# LUXRAYS_CUSTOM_CONFIG
-# C:/Users/David/Dati/OpenCL/LuxRays-64bit/luxrays/cmake/SpecializedConfig/Config_Dade-win64.cmake
+# Sfera_CUSTOM_CONFIG
+# C:/Users/David/Dati/OpenCL/Sfera-64bit/sfera/cmake/SpecializedConfig/Config_Dade-win64.cmake
 # 
 # Boost:
 #  "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat"
@@ -17,23 +17,18 @@ MESSAGE(STATUS "Using Dade's Win64 Configuration settings")
 
 set(CMAKE_BUILD_TYPE "Release")
 
-set(BUILD_LUXMARK TRUE) # This will require QT
-
-set(ENV{QTDIR} "C:/Qt/4.7.1")
-
-set(FREEIMAGE_SEARCH_PATH "C:/Users/David/Dati/OpenCL/LuxRays-64bit/FreeImage")
+set(FREEIMAGE_SEARCH_PATH "C:/Users/David/Dati/OpenCL/Sfera-64bit/FreeImage")
 set(FreeImage_INC_SEARCH_PATH "${FREEIMAGE_SEARCH_PATH}/Dist")
 set(FreeImage_LIB_SEARCH_PATH "${FREEIMAGE_SEARCH_PATH}/Dist")
 
-set(BOOST_SEARCH_PATH         "C:/Users/David/Dati/OpenCL/LuxRays-64bit/boost_1_43_0")
+set(BOOST_SEARCH_PATH         "C:/Users/David/Dati/OpenCL/Sfera-64bit/boost_1_43_0")
 set(BOOST_LIBRARYDIR          "${BOOST_SEARCH_PATH}/stage/boost/lib")
 
 set(OPENCL_SEARCH_PATH        "C:/Program Files (x86)/AMD APP")
 set(OPENCL_LIBRARYDIR         "${OPENCL_SEARCH_PATH}/lib/x86_64")
 
-set(GLUT_SEARCH_PATH          "C:/Users/David/Dati/OpenCL/LuxRays-64bit/freeglut-2.6.0")
-set(GLUT_LIBRARYDIR           "${GLUT_SEARCH_PATH}/VisualStudio2008Static/x64/Release")
-ADD_DEFINITIONS(-DFREEGLUT_STATIC)
-
-set(GLEW_SEARCH_PATH          "C:/Users/David/Dati/OpenCL/LuxRays-64bit/glew-1.6.0")
+set(GLEW_SEARCH_PATH          "C:/Users/David/Dati/OpenCL/Sfera-64bit/glew-1.6.0")
 ADD_DEFINITIONS(-DGLEW_BUILD)
+
+set(BULLET_SEARCH_PATH         "C:/Users/David/Dati/OpenCL/Sfera-64bit/bullet-2.78/src")
+set(BULLET_LIBRARYDIR          "C:/Users/David/Dati/OpenCL/Sfera-64bit/bullet-bin/lib/Release")
